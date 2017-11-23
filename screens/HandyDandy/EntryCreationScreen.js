5e005680224677b0a545834d136fffac14eff3f0
Jameson Welling <jamowelling@gmail.com>
2017-11-22 20:55:02 -0700

    add some styling

--- screens/HandyDandy/EntryCreationScreen.js
+++ screens/HandyDandy/EntryCreationScreen.js
@@ -2,30 +2,33 @@ import React, { Component } from 'react';
 import {
   View,
   TextInput,
-  Dimensions,
   // AsyncStorage
 } from 'react-native';
 
 class EntryCreationScreen extends Component <{}> {
   state = {
-    titleText: Date().toLocaleString('en-US'),
-    bodyText: 'Placeholder',
+    titleText: '',
+    bodyText: '',
   };
 
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <View style={{flex: 2 }}>
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 2 , borderWidth: 1, borderColor: 'blue' }}>
           <TextInput
             onChangeText={titleText => this.setState({ titleText })}
             value={this.state.titleText}
+            defaultValue={Date().toLocaleString('en-US')}
+            placeholder={Date().toLocaleString('en-US', { hour12: true })}
           />
         </View>
-        <View style={{ flex: 5, width: Dimensions.get('window').width }}>
+        <View style={{ flex: 5, borderWidth: 1, borderColor: 'red' }}>
           <TextInput
             onChangeText={bodyText => this.setState({ bodyText })}
             value={this.state.bodyText}
-            style={{ flex: 1, textAlign: 'center' }}
+            placeholder='Placeholder'
+            autoFocus
+            multiline
           />
         </View>
       </View>
