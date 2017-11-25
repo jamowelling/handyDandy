981911a0919493b2b1b3ea79cf2b081c8c4b42a9
Jameson Welling <jamowelling@gmail.com>
2017-11-25 11:30:54 -0700

    create and register entry display screen, pressing an entry now pushes to the entry display screen and passes props

---
+++ screens/EntryDisplay/EntryDisplayScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EntryDisplayScreen extends Component <{}> {
+  render() {
+    return (
+      <View>
+        <Text>Entry Display Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default EntryDisplayScreen;
