c9b9efbe40341b2499ad60268654d1b1bfd69aa6
Jameson Welling <jamowelling@gmail.com>
2017-12-12 19:44:54 -0700

    add update entries to action creators

--- src/actions/entryActions.js
+++ src/actions/entryActions.js
@@ -3,3 +3,14 @@
 */
 
 export const UPDATE_ENTRIES = 'UPDATE_ENTRIES';
+
+/*
+* action creators
+*/
+
+export function updateEntries(entries) {
+  return {
+    type: UPDATE_ENTRIES,
+    payload: entries,
+  };
+}
