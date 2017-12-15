216cd2502bf69306eda0feed43ac4b44fa12b4fd
Jameson Welling <jamowelling@gmail.com>
2017-12-14 21:21:07 -0700

    connect handy dandy screen to the store, implement some boilerplate synchronous action creators for entries retrieval

--- src/actions/entryActions.js
+++ src/actions/entryActions.js
@@ -1,16 +1,31 @@
-/*
-* action types
-*/
-
-export const UPDATE_ENTRIES = 'UPDATE_ENTRIES';
 
 /*
-* action creators
+* synchronous action creators
 */
 
-export function updateEntries(entries) {
+export const RETRIEVE_ENTRIES_BEGIN = 'RETRIEVE_ENTRIES_BEGIN';
+export function retrieveEntriesBegin() {
   return {
-    type: UPDATE_ENTRIES,
-    payload: entries,
+    type: RETRIEVE_ENTRIES_BEGIN,
   };
 }
+
+export const RETRIEVE_ENTRIES_FAILURE = 'RETRIEVE_ENTRIES_FAILURE';
+export function retrieveEntriesFailure() {
+  return {
+    type: RETRIEVE_ENTRIES_FAILURE,
+    error: 'Something went wrong...',
+  };
+}
+
+export const RETRIEVE_ENTRIES_SUCCESS = 'RETRIEVE_ENTRIES_SUCCESS';
+export function retrieveEntriesSuccess(entries) {
+  return {
+    type: RETRIEVE_ENTRIES_SUCCESS,
+    entries,
+  };
+}
+
+/*
+* asnchronous action creators
+*/
